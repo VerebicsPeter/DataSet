6e1576adce504c81cefa46b664ced71819ba2d20
VerebicsPeter <peterverebics@gmail.com>
2023-10-13 11:35:24 +0200

    extracted method for matching pattern

--- source/scripts/common_pattern_trasformations.py
+++ source/scripts/common_pattern_trasformations.py
@@ -17,7 +17,7 @@ def node_is_assignment_to(node, name: str) -> bool:
         return node.target.name.value == name
     return False
 
-def node_get_last_by_name(node, name: str, predicate = lambda x, y: True):
+def get_last_node_before(node, name = None, predicate = lambda _node, _name: True):
     index = node.index_on_parent
     if index is None: return None
     last = None
@@ -27,45 +27,57 @@ def node_get_last_by_name(node, name: str, predicate = lambda x, y: True):
 
 # Transformations:
 
+def t_for_to_listcomprehension_match(for_node) -> bool:
+    if node_count_children(for_node,'IfelseblockNode') != 1:
+        return False
+    if (node_count_children(for_node.value.find('IfelseblockNode'),'ElifNode') or
+        node_count_children(for_node.value.find('IfelseblockNode'),'ElseNode')):
+        return False
+    if (node_count_children(
+        for_node.
+        value.find('IfelseblockNode').
+        value.find('IfNode'),
+        'AtomtrailersNode') != 1
+        ):
+        return False
+    if (node_count_children(
+        for_node.
+        value.find('IfelseblockNode').
+        value.find('IfNode').
+        value.find('AtomtrailersNode'),
+        'name') != 3):
+        return False
+    return True
+
 def t_for_to_listcomprehension(for_node) -> tuple | None:
+    if not t_for_to_listcomprehension_match(for_node): return None
+    
     iterator, target = for_node.iterator, for_node.target
 
-    if node_count_children(for_node,'IfelseblockNode') != 1:
-        return None
-    if_else_block = for_node.value.find('IfelseblockNode')
-
-    if node_count_children(if_else_block,'ElseNode'):
-        return None
-    if_node = if_else_block.value.find('IfNode')
-
-    if node_count_children(if_node,'AtomtrailersNode') != 1:
-        return None
-    atom_trailers_node = if_node.value.find('AtomtrailersNode')
-
-    if node_count_children(atom_trailers_node,'name') != 3:
-        return None
+    # get nodes necessary for transformation
+    if_node = for_node.value.find('IfelseblockNode').value.find('IfNode')
+    # namenodes is the list of name nodes in the atom trailers node inside if node
     namenodes = if_node.value.find_all('name')
-
-    test = if_node.test
+    # value check for name nodes
+    if namenodes[1].name.value != 'append':
+        return None
+    if namenodes[2].name.value != iterator.name.value:
+        return None
     name = namenodes[0].name.value
-    assignment_node = node_get_last_by_name(for_node, name, node_is_assignment_to)
-    
+
+    assignment_node = get_last_node_before(for_node, name, node_is_assignment_to)
+    last_mention    = get_last_node_before(for_node, name, node_mentions)
+    # type and value checks for assignment node
     if assignment_node is None:
         return None
     if not isinstance(assignment_node.value, rb.ListNode):
         return None
     if len(assignment_node.list.value):
         return None
-
-    last_mention = node_get_last_by_name(for_node, name, node_mentions)
-
     if assignment_node is not last_mention:
         return None
-    if namenodes[1].name.value != 'append':
-        return None
-    if namenodes[2].name.value != iterator.name.value:
-        return None
-
+    
+    test = if_node.test
     result = f'[{iterator} for {iterator} in {target} if {test}]'
     return (assignment_node, result)
 
--- source/scripts/help.py
+++ source/scripts/help.py
@@ -11,16 +11,14 @@ my_empty_list = []
 """
 
 red = rb.RedBaron(source)
-print(red.help(deep=True))
+#print(red.help(deep=True))
 #print("root node's parent:", red.parent) # None
 #ifelseblock = red.find('IfelseblockNode')
 #print(ifelseblock.help(deep=True))
 
 assignment = red.find('AssignmentNode')
 #print(assignment)
+
 list_node = assignment.find('ListNode')
 #print('listnode value and type:',list_node.value,type(list_node.value))
 #print(list_node.value.help(deep=True))
-
-#print(assignment.value.help(deep=True))
-#print('Value of the list:', assignment.value.list)
