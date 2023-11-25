48469bee21e1304227eb0014d3ced6d96514160b
VerebicsPeter <peterverebics@gmail.com>
2023-11-25 07:40:04 +0100

    refactored ui

--- source/application/main.py
+++ source/application/main.py
@@ -1,43 +1,55 @@
+import ast
+
 import tkinter as tk
 
-import ast
+from tkinter import ttk
 
 from graphviz import Digraph
 
 
-class ApplicationGUI:
+class Menu(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self._menubar = tk.Menu(parent)
+        self.filemenu = tk.Menu(self._menubar, tearoff=0)
+        self.filemenu.add_command(label="Close", command=self.on_closing)
+        self._menubar.add_cascade(label="File", menu=self.filemenu, font=('lucida',10))
+        self.parent = parent
+    
+    def on_closing(self):
+        self.parent.destroy()
 
-    def __init__(self) -> None:
-        self.root = tk.Tk()
-        self.root.geometry("800x600")
-        self.root.title("Equivalent Transformations")
 
-        self.menubar = tk.Menu(self.root)
-        self.appmenu = tk.Menu(self.menubar, tearoff=0)
-        self.appmenu.add_command(label="Close", command=self.on_closing)
-        self.menubar.add_cascade(label="App", menu=self.appmenu)
+class SourceWidget(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        
+        self.source_label = tk.Label(parent, text="Source", font=('lucida', 16))
+        self.source_label.grid(
+            row=0, column=0, padx=10, pady=10, columnspan=2
+        )
+        self.source_textbox = tk.Text(parent, height=21, width=42, font=('courier', 16))
+        self.source_textbox.grid(
+            row=1, column=0, padx=10, pady=10, columnspan=2
+        )
+        self.source_ast_button = tk.Button(parent, text="Show AST", font=('lucida', 14), command=self.show_ast)
+        self.source_ast_button.grid(
+            row=2, column=0, padx=10, pady=10
+        )
+        
+        self.t_button = tk.Button(parent, text="Transform", font=('lucida', 16)) #command=)
+        self.t_button.grid(
+            row=2, column=1, padx=10, pady=10
+        )
 
-        self.root.config(menu=self.menubar)
-
-        self.label = tk.Label(self.root, text="Source", font=('Arial', 20))
-        self.label.pack(padx=20, pady=20)
-
-        self.textbox = tk.Text(self.root, height=20, font=('Consolas', 16))
-        self.textbox.pack(padx=10, pady=10)
-
-        self.button = tk.Button(self.root, text="Get AST", font=(
-            'Arial', 16), command=self.show_ast)
-        self.button.pack(padx=10, pady=10)
-
-        self.root.mainloop()
-
+        self.parent = parent
+        
     def show_ast(self):
-        text = self.textbox.get('1.0', tk.END)
-
+        text = self.source_textbox.get('1.0', tk.END)
+        
         parsed = ast.parse(text)
 
-        if not parsed:
-            print('Error while parsing.')
+        if not parsed: print('Error while parsing.')
         
         print('-'*150)
         print(parsed)
@@ -47,8 +59,60 @@ class ApplicationGUI:
         
         DotGrapMaker.make(parsed)
 
-    def on_closing(self):
-        self.root.destroy()
+
+class ResultWidget(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        
+        self.result_label = tk.Label(parent, text="Result", font=('lucida', 16))
+        self.result_label.grid(
+            row=0, column=2, padx=10, pady=10, columnspan=2
+        )
+        self.result_textbox = tk.Text(parent, height=21, width=42, font=('courier', 16))
+        self.result_textbox.grid(
+            row=1, column=2, padx=10, pady=10, columnspan=2
+        )
+        self.result_ast_button = tk.Button(parent, text="Show AST", font=('lucida', 14), command=self.show_ast)
+        self.result_ast_button.grid(
+            row=2, column=2, padx=10, pady=10, columnspan=2
+        )
+    
+    def show_ast(self):
+        text = self.result_textbox.get('1.0', tk.END)
+        
+        parsed = ast.parse(text)
+
+        if not parsed: print('Error while parsing.')
+        
+        print('-'*150)
+        print(parsed)
+        print('-'*150)
+        print(ast.dump(parsed, indent=2))
+        print('-'*150)
+        
+        DotGrapMaker.make(parsed)
+
+
+class App(tk.Tk):
+
+    def __init__(self) -> None:
+        # setup
+        super().__init__()
+        self.geometry("1150x640")
+        self.maxsize(1150,640)
+        self.minsize( 640,480)
+        self.title("Equivalent Transformations")
+        
+        # menu
+        self.menu = Menu(self)
+        self.config(menu=self.menu._menubar)
+        
+        # widgets
+        self.source_widget = SourceWidget(self)
+        self.result_widget = ResultWidget(self)
+        
+        # run
+        self.mainloop()
 
 
 class DotGrapMaker:
@@ -76,5 +140,4 @@ class DotGrapMaker:
 
 
 if __name__ == "__main__":
-
-    app = ApplicationGUI()
+    app = App()
